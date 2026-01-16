95d053cb902e4f947e70956fbd371f8d0fe1efde
tempa37 <ron05051980@gmail.com>
2026-01-16 07:39:17 +0200

    Store TEU mode as number

--- MB_Pythom/Sh.py
+++ MB_Pythom/Sh.py
@@ -239,7 +239,7 @@ def export_posts_to_excel(post_count, filename, scan_ip, scan_id, output_callbac
                         mode_label = "-"
                         address_value = "-"
                     else:
-                        mode_label = str(channel_bytes[2])
+                        mode_label = channel_bytes[2]
                 elif channel_bytes[0] == 0x34:
                     adjusted_value = channel_bytes[2] - 127
                     mode_label = f"0 = {adjusted_value}"
